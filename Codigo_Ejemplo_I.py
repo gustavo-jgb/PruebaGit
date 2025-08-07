1f721858eb1f2d9a94906f6ed1c488ef62f9b407
GUS <gustavojose.garciabalza@outlook.com>
2025-08-06 19:03:40 -0500

    Se agregro una subcarpeta co un archivo al repositorio, y se agrego un modulo al codigo fuente

--- Codigo_Ejemplo_I.py
+++ Codigo_Ejemplo_I.py
@@ -1,1 +1,5 @@
+def mensaje():
+    return "Este es un mensaje enviado desde una función en Python"
+
 print("Hola Gustavo")
+print(mensaje())
---
+++ Subcarpeta/Codigo_Ejemplo_II.py
@@ -0,0 +1,1 @@
+print("Hola este es otro ejemplo de código en Python")
